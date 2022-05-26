edbc803b2e6255af836cd3694005c2eb6db05da2
mehmet yiğit <56845461+mehmetygt@users.noreply.github.com>
2022-05-26 13:44:36 +0300

    Add files via upload

---
+++ DES/src/des/DESTestClass.java
@@ -0,0 +1,28 @@
+package des;
+
+public class DESTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		DES des = new DES();
+		
+		String key = "secretkey";
+		String cipher = des.encrypt(key, "This is a secret message being encrypted by DES and isn't it awesome that it finally works?");
+		
+		
+		System.out.println("Encrypting message with key: 'secretkey'\nMessage:\n" + 
+				"This is a secret message being encrypted by DES and isn't it awesome that it finally works?\nCiphertext (Hex):");
+		System.out.println(cipher);
+		System.out.println("Decrypting ciphertext with key: 'secretkey'\nDecrypted Plaintext:");
+		String plain = des.decrypt(key, cipher);
+		System.out.println(plain);
+		
+		System.out.println("\nTriple DES: \nplaintext: WHOO TRIPLE DES!!\nkeys:\nchimichanga, raptor, snapdragon\nciphertext:");
+		System.out.println(des.encrypt("snapdragon", des.decrypt("raptor", des.encrypt("chimichanga", "WHOO TRIPLE DES!!"))));
+		System.out.println("Decryption:");
+		System.out.println(des.decrypt("chimichanga", des.encrypt("raptor", des.decrypt("snapdragon", "WHOO TRIPLE DES!!"))));
+
+	}
+
+}
